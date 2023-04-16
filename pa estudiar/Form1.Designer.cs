aef33db312ffa8112d6296ed01637d4707cccccf
Samuel Cristian Cortez Concha <samitoskater22@gmail.com>
2023-04-16 18:30:16 -0400

    Agregar archivos de proyecto.

---
+++ pa estudiar/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace pa_estudiar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtDivision = new System.Windows.Forms.TextBox();
+            this.txtPotencia = new System.Windows.Forms.TextBox();
+            this.txtRaiz = new System.Windows.Forms.TextBox();
+            this.txtMultiplicacion = new System.Windows.Forms.TextBox();
+            this.txtResta = new System.Windows.Forms.TextBox();
+            this.txtSuma = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtRaiz2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(644, 521);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtRaiz2);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.txtDivision);
+            this.groupBox2.Controls.Add(this.txtPotencia);
+            this.groupBox2.Controls.Add(this.txtRaiz);
+            this.groupBox2.Controls.Add(this.txtMultiplicacion);
+            this.groupBox2.Controls.Add(this.txtResta);
+            this.groupBox2.Controls.Add(this.txtSuma);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(24, 236);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(585, 272);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtDivision
+            // 
+            this.txtDivision.Location = new System.Drawing.Point(421, 32);
+            this.txtDivision.Name = "txtDivision";
+            this.txtDivision.ReadOnly = true;
+            this.txtDivision.Size = new System.Drawing.Size(100, 20);
+            this.txtDivision.TabIndex = 17;
+            // 
+            // txtPotencia
+            // 
+            this.txtPotencia.Location = new System.Drawing.Point(421, 90);
+            this.txtPotencia.Name = "txtPotencia";
+            this.txtPotencia.ReadOnly = true;
+            this.txtPotencia.Size = new System.Drawing.Size(100, 20);
+            this.txtPotencia.TabIndex = 16;
+            this.txtPotencia.TextChanged += new System.EventHandler(this.txtPotencia_TextChanged);
+            // 
+            // txtRaiz
+            // 
+            this.txtRaiz.Location = new System.Drawing.Point(421, 157);
+            this.txtRaiz.Name = "txtRaiz";
+            this.txtRaiz.ReadOnly = true;
+            this.txtRaiz.Size = new System.Drawing.Size(100, 20);
+            this.txtRaiz.TabIndex = 15;
+            // 
+            // txtMultiplicacion
+            // 
+            this.txtMultiplicacion.Location = new System.Drawing.Point(89, 150);
+            this.txtMultiplicacion.Name = "txtMultiplicacion";
+            this.txtMultiplicacion.ReadOnly = true;
+            this.txtMultiplicacion.Size = new System.Drawing.Size(100, 20);
+            this.txtMultiplicacion.TabIndex = 14;
+            // 
+            // txtResta
+            // 
+            this.txtResta.Location = new System.Drawing.Point(89, 93);
+            this.txtResta.Name = "txtResta";
+            this.txtResta.ReadOnly = true;
+            this.txtResta.Size = new System.Drawing.Size(100, 20);
+            this.txtResta.TabIndex = 13;
+            this.txtResta.TextChanged += new System.EventHandler(this.txtResta_TextChanged);
+            // 
+            // txtSuma
+            // 
+            this.txtSuma.Location = new System.Drawing.Point(89, 32);
+            this.txtSuma.Name = "txtSuma";
+            this.txtSuma.ReadOnly = true;
+            this.txtSuma.Size = new System.Drawing.Size(100, 20);
+            this.txtSuma.TabIndex = 12;
+            this.txtSuma.TextChanged += new System.EventHandler(this.txtSuma_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(302, 157);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Raiz del Numero 1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(339, 96);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Potencia";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(339, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Division";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Multiplicacion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Suma";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Resta";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNumero2);
+            this.groupBox1.Controls.Add(this.txtNumero1);
+            this.groupBox1.Controls.Add(this.btnLimpiar);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(24, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(585, 193);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operaciones";
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(138, 108);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero2.TabIndex = 5;
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(138, 47);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero1.TabIndex = 4;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(342, 111);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(342, 49);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(302, 212);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Raiz del Numero 2";
+            // 
+            // txtRaiz2
+            // 
+            this.txtRaiz2.Location = new System.Drawing.Point(421, 205);
+            this.txtRaiz2.Name = "txtRaiz2";
+            this.txtRaiz2.ReadOnly = true;
+            this.txtRaiz2.Size = new System.Drawing.Size(100, 20);
+            this.txtRaiz2.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 520);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDivision;
+        private System.Windows.Forms.TextBox txtPotencia;
+        private System.Windows.Forms.TextBox txtRaiz;
+        private System.Windows.Forms.TextBox txtMultiplicacion;
+        private System.Windows.Forms.TextBox txtResta;
+        private System.Windows.Forms.TextBox txtSuma;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRaiz2;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ pa estudiar/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pa_estudiar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double num1, num2, suma, resta, multiplicacion, division, potencia, raiz,raiz2;
+            num1 = double.Parse(txtNumero1.Text);
+            num2 = Convert.ToDouble(txtNumero2.Text);
+            suma = num1 + num2;
+            resta = num1 - num2;
+            multiplicacion = num1 * num2;
+            division = num1 / num2;
+            potencia = Math.Pow(num1, num2);
+            raiz = Math.Sqrt(num1);
+            raiz2 = Math.Sqrt(num2);
+            txtSuma.Text = Convert.ToString(suma);
+            txtResta.Text = Convert.ToString(resta);
+            txtMultiplicacion.Text = Convert.ToString(multiplicacion);
+            txtDivision.Text = Convert.ToString(division);
+            txtPotencia.Text = Convert.ToString(potencia);
+            txtRaiz.Text = Convert.ToString(raiz);
+            txtRaiz2.Text = Convert.ToString(raiz2);
+        }
+
+        private void txtSuma_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtResta_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtSuma.Clear();
+            txtResta.Clear();
+            txtMultiplicacion.Clear();
+            txtDivision.Clear();
+            txtPotencia.Clear();
+            txtRaiz.Clear();
+        }
+
+        private void txtPotencia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
